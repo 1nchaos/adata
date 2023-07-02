cca253b6b288d689bf68abba733fdaab56b70546
1nchaos <9527@1nchaos.com>
2023-07-02 09:08:20 +0800

    fixed fix market bug

--- tests/adata/stock/makert_test.py
+++ tests/adata/stock/makert_test.py
@@ -4,8 +4,14 @@
 @author: 1nchaos
 @date: 2023/7/2 07:51
 """
-from adata import stock
+import adata
 
 if __name__ == '__main__':
-    res_df = stock.market.get_market(stock_code='000100', k_type=1, start_date='2021-01-01')
+    res_df = adata.stock.market.get_market(stock_code='000100', k_type=1, start_date='2021-01-01')
+    print(res_df)
+    res_df = adata.stock.market.get_market(stock_code='000001', start_date='2021-01-01', k_type=1)
+    print(res_df)
+    res_df = adata.stock.market.get_market_min(stock_code='000001')
+    print(res_df)
+    res_df = adata.stock.market.list_market_current(code_list=['000001', '600001', '000795', '872925'])
     print(res_df)
