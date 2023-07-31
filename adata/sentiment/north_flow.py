1c6425af2154976ac5eff7e1d80168b93af2c3cc
1nchaos <yc369.cool@163.com>
2023-08-01 00:18:14 +0800

    update
    north_flow history

--- adata/sentiment/north_flow.py
+++ adata/sentiment/north_flow.py
@@ -4,8 +4,7 @@
 北向资金
 来源：东方财富
 https://data.eastmoney.com/hsgt/index.html
-https://datacenter-web.eastmoney.com/api/data/v1/get?callback=jQuery112304138107938255364_1690533543847&sortColumns=TRADE_DATE&sortTypes=-1&pageSize=1000&pageNumber=1&reportName=RPT_MUTUAL_DEAL_HISTORY&columns=ALL&source=WEB&client=WEB&filter=(MUTUAL_TYPE%3D%22001%22)
-
+https://datacenter-web.eastmoney.com/api/data/v1/get?callback=jQuery112307442704592215257_1690813516314&sortColumns=TRADE_DATE&sortTypes=-1&pageSize=10&pageNumber=2&reportName=RPT_MUTUAL_DEAL_HISTORY&columns=ALL&source=WEB&client=WEB&filter=(MUTUAL_TYPE="001")
 同花顺
 https://data.hexin.cn/market/hsgtApi/method/hsgtData/
 
@@ -16,6 +15,7 @@ https://data.hexin.cn/market/hsgtApi/method/hsgtData/
 import copy
 import datetime
 import json
+import math
 
 import pandas as pd
 
@@ -26,14 +26,95 @@ from adata.common.headers import ths_headers
 
 
 class NorthFlow(BaseThs):
-    __NORTH_FLOW_MIN_COLUMNS = ['trade_time', 'hgt', 'sgt', 'tgt']
+    __NORTH_FLOW_COLUMNS = ['trade_date', 'net_hgt', 'buy_hgt', 'sell_hgt', 'net_sgt', 'buy_sgt', 'sell_sgt',
+                            'net_tgt', 'buy_tgt', 'sell_tgt']
+
+    __NORTH_FLOW_MIN_COLUMNS = ['trade_time', 'net_hgt', 'net_sgt', 'net_tgt']
     __NORTH_FLOW_CURRENT_COLUMNS = __NORTH_FLOW_MIN_COLUMNS
 
     def __init__(self) -> None:
         super().__init__()
 
     def north_flow(self, start_date=None):
-        pass
+        """
+        获取北向资金历史的数据，开始时间到最新的历史数据，
+        :param start_date: 开始时间，最早：1017-01-01
+        :return:
+        'trade_date'：交易日期
+        'net_hgt', ：沪港通净买入金额（元）
+        'buy_hgt', ：沪港通净买入金额（元）
+        'sell_hgt', ：沪港通净买入金额（元）
+        'net_sgt', ：深港通净买入金额（元）
+        'buy_sgt', ：深港通净买入金额（元）
+        'sell_sgt', ：深港通卖出金额（元）
+        'net_tgt', ：合计净买入金额（元）
+        'buy_tgt', ：合计买入金额（元）
+        'sell_tgt' ：合计卖出金额（元）
+        """
+
+        return self.__north_flow_east(start_date=start_date)
+
+    def __north_flow_east(self, start_date=None):
+        """
+        https://datacenter-web.eastmoney.com/api/data/v1/get?callback=jQuery112307442704592215257_1690813516314&sortColumns=TRADE_DATE&sortTypes=-1&pageSize=10&pageNumber=2&reportName=RPT_MUTUAL_DEAL_HISTORY&columns=ALL&source=WEB&client=WEB&filter=(MUTUAL_TYPE="001")
+        """
+        if start_date:
+            start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+            date_min = datetime.datetime.strptime('2017-01-01', '%Y-%m-%d')
+            if start_date < date_min:
+                start_date = date_min
+        curr_page = 1
+        data = []
+        while curr_page < 18:
+            url = f'https://datacenter-web.eastmoney.com/api/data/v1/get?callback=jQuery112307442704592215257_1690813516314' \
+                  f'&sortColumns=TRADE_DATE&sortTypes=-1&pageSize=1000&pageNumber={curr_page}&' \
+                  f'reportName=RPT_MUTUAL_DEAL_HISTORY&columns=ALL&source=WEB&client=WEB&'
+            sgt_url = f"""{url}filter=(MUTUAL_TYPE="001")"""
+            hgt_url = f"""{url}filter=(MUTUAL_TYPE="003")"""
+
+            sgt = requests.request('get', sgt_url, headers={}, proxies={}).text.replace('null', '0')
+            hgt = requests.request('get', hgt_url, headers={}, proxies={}).text.replace('null', '0')
+            sgt_json = json.loads(sgt[sgt.index('{'):-2])
+            hgt_json = json.loads(hgt[hgt.index('{'):-2])
+            sgt_data = sgt_json["result"]["data"]
+            hgt_data = hgt_json["result"]["data"]
+            if not sgt_data:
+                break
+            is_end = False
+            for i in range(len(sgt_data)):
+                if not start_date and i >= 30:
+                    is_end = True
+                    break
+                if start_date:
+                    date_min = datetime.datetime.strptime(hgt_data[i]['TRADE_DATE'], '%Y-%m-%d %H:%M:%S')
+                    if start_date > date_min:
+                        is_end = True
+                        break
+
+                data.append({
+                    'trade_date': hgt_data[i]['TRADE_DATE'],
+                    'net_hgt': math.ceil(hgt_data[i]['NET_DEAL_AMT'] * 1000000),
+                    'buy_hgt': math.ceil(hgt_data[i]['BUY_AMT'] * 1000000),
+                    'sell_hgt': math.ceil(hgt_data[i]['SELL_AMT'] * 1000000),
+
+                    'net_sgt': math.ceil(sgt_data[i]['NET_DEAL_AMT'] * 1000000),
+                    'buy_sgt': math.ceil(sgt_data[i]['BUY_AMT'] * 1000000),
+                    'sell_sgt': math.ceil(sgt_data[i]['SELL_AMT'] * 1000000),
+
+                    'net_tgt': math.ceil((hgt_data[i]['NET_DEAL_AMT'] + sgt_data[i]['NET_DEAL_AMT']) * 1000000),
+                    'buy_tgt': math.ceil((hgt_data[i]['BUY_AMT'] + sgt_data[i]['BUY_AMT']) * 1000000),
+                    'sell_tgt': math.ceil((hgt_data[i]['SELL_AMT'] + sgt_data[i]['SELL_AMT']) * 1000000)
+                })
+
+            if is_end:
+                break
+            curr_page += 1
+
+        # 封装数据
+        result_df = pd.DataFrame(data=data, columns=self.__NORTH_FLOW_COLUMNS)
+        result_df['trade_date'] = pd.to_datetime(result_df['trade_date']).dt.strftime('%Y-%m-%d')
+
+        return result_df[self.__NORTH_FLOW_COLUMNS]
 
     def north_flow_min(self):
         """
@@ -66,7 +147,8 @@ class NorthFlow(BaseThs):
         sgt_list = result_json['sgt']
         data = []
         for i in range(len(time_list)):
-            row = [time_list[i], hgt_list[i], sgt_list[i], float(hgt_list[i]) + float(sgt_list[i])]
+            row = [time_list[i], math.ceil(hgt_list[i] * 100000000), math.ceil(sgt_list[i] * 100000000),
+                   math.ceil((hgt_list[i] + sgt_list[i]) * 100000000)]
             data.append(row)
         # 3. 封装数据
         result_df = pd.DataFrame(data=data, columns=self.__NORTH_FLOW_CURRENT_COLUMNS)
@@ -87,6 +169,6 @@ class NorthFlow(BaseThs):
 
 
 if __name__ == '__main__':
-    print(NorthFlow().north_flow('2022-01-01'))
     print(NorthFlow().north_flow_min())
     print(NorthFlow().north_flow_current())
+    print(NorthFlow().north_flow(start_date='2000-11-01'))
