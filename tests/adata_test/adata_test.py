a0b0cc6716475e6447dbe862335d912ca5b9b7e1
1nchaos <9527@1nchaos.com>
2023-08-01 15:54:16 +0800

    update
    test

--- tests/adata_test/adata_test.py
+++ tests/adata_test/adata_test.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     # defaultTestLoader测试加载器：包含加载测试用例的方法;使用discover()方法来自动识别并添加测试用例(多个)
     suite = unittest.defaultTestLoader.discover("", '*_test.py')
     # 生成一个本地时间 格式如20220413091429，年月时时分秒
-    time_str = time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime(time.time()))
+    time_str = time.strftime('%Y-%m-%d_%H%M%S', time.localtime(time.time()))
     # 生成的报告的名字和目录
     filename = f"./AData-{version}自动化测试报告-" + time_str + ".html"
     # 打开这个文件
