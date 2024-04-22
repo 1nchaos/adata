52aee523675368081ce54a8786747741640126b2
1nchaos <yc369.cool@163.com>
2024-04-22 23:26:48 +0800

    update bond

--- adata/bond/market/bond_market.py
+++ adata/bond/market/bond_market.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-@desc: 
+@desc:
+http://www.iwencai.com/unifiedwap/result?typed=1&preParams=&ts=1&f=1&qs=1&selfsectsn=&querytype=&searchfilter=&tid=stockpick&w=127093
+
 @author: 1nchaos
 @time:2023/4/5
 @log: 
