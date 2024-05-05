a103a7cac5264465349714d55b9c4f740fe0cdd7
1nchaos <yc369.cool@163.com>
2024-05-05 23:37:29 +0800

    add hot

---
+++ adata/bond/market/bond_market_template.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+@desc: readme
+@author: 1nchaos
+@time: 2024/05/05
+@log: change log
+"""
+
+
+class BondMarketTemplate(object):
+    """
+    债券行情
+    """
+    _MARKET_CURRENT_COLUMNS = ['bond_code', 'bond_name', 'price', 'open', 'high', 'low', 'pre_close', 'change',
+                               'change_pct', 'volume', 'amount', 'time']
+    _MARKET_COLUMNS = ['bond_code', 'bond_name', 'open', 'close', 'volume', 'high', 'low', 'amount',
+                       'change', 'change_pct', 'pre_close']
+    _MARKET_MIN_COLUMNS = ['bond_code', 'trade_time', 'price', 'change', 'change_pct', 'volume', 'avg_price', 'amount']
+    _MARKET_FIVE_COLUMNS = ['bond_code', 'bond_name', 's5', 'sv5', 's4', 'sv4', 's3', 'sv3', 's2', 'sv2', 's1', 'sv1',
+                            'b1', 'bv1', 'b2', 'bv2', 'b3', 'bv3', 'b4', 'bv4', 'b5', 'bv5']
+    _MARKET_BAR_COLUMNS = ['bond_code', 'trade_time', 'price', 'volume', 'bs_type']
+
+    # def get_market(self, stock_code: str = '000001', start_date='1990-01-01', k_type=1, adjust_type: int = 1):
+    #     """
+    #     获取单个股票的行情
+    #     :param stock_code: 股票代码
+    #     :param start_date: 开始时间
+    #     :param k_type: k线类型：1.日；2.周；3.月 默认：1 日k
+    #     :param adjust_type: k线复权类型：0.不复权；1.前复权；2.后复权 默认：1 前复权 （目前：只有前复权,作为股票交易已经可用）
+    #     :return: k线行情数据
+    #     _MARKET_COLUMNS
+    #     """
+    #     pass
+    #
+    # def get_market_min(self, stock_code: str = '000001'):
+    #     """
+    #     获取单个股票的今日分时行情
+    #     :param stock_code: 股票代码
+    #     :return: 当日分钟行情数据
+    #     _MARKET_MIN_COLUMNS
+    #     """
+    #     pass
+
+    def list_market_current(self, code_list=None):
+        """
+        获取多个可转债的最新行情信息
+        :param code_list: 可转债代码
+        :return: 当前最新的行情价格信息
+        _MARKET_CURRENT_COLUMNS
+        """
+        pass
+
+    # def get_market_five(self, stock_code: str = '000001'):
+    #     """
+    #     获取单个股票的5档行情
+    #     :param stock_code: 股票代码
+    #     :return: 最新的五档行情
+    #     """
+    #     pass
+
+    # def list_market_five(self, code_list=None):
+    #     """
+    #      https://web.sqt.gtimg.cn/q=sh601666,sh600691
+    #     :param code_list: 股票代码列表
+    #     :return:
+    #     """
+    #     pass
+    #
+    # def get_market_bar(self, stock_code: str = '000001'):
+    #     """
+    #     获取单个股票的分时成交
+    #     :param stock_code: 股票代码
+    #     :return: 最新当天的分时成交
+    #     """
+    #     pass
--- adata/sentiment/hot.py
+++ adata/sentiment/hot.py
@@ -9,3 +9,63 @@ https://eq.10jqka.com.cn/frontend/thsTopRank/index.html?fontzoom=no&client_useri
 @time: 2024/4/29
 @log: change log
 """
+import pandas as pd
+
+from adata.common.base.base_ths import BaseThs
+from adata.common.exception.exception_msg import THS_IP_LIMIT_RES, THS_IP_LIMIT_MSG
+from adata.common.utils import requests
+
+
+class Hot(BaseThs):
+    """热门榜单"""
+
+    # 东方财富人气榜
+    def pop_rank_100_east(self):
+        """
+        东方财富人气榜100
+        http://guba.eastmoney.com/rank/
+        """
+        # 1.url
+        url = "https://emappdata.eastmoney.com/stockrank/getAllCurrentList"
+
+        # 2. 请求数据
+        params = {"appId": "appId01", "globalId": "786e4c21-70dc-435a-93bb-38",
+                  "marketType": "", "pageNo": 1, "pageSize": 100, }
+        res = requests.request(method='post', url=url, json=params).json()
+        df = pd.DataFrame(res["data"])
+
+        df["mark"] = ["0" + "." + item[2:] if "SZ" in item else "1" + "." + item[2:]
+                      for item in df["sc"]]
+        ",".join(df["mark"]) + "?v=08926209912590994"
+        params = {"ut": "f057cbcbce2a86e2866ab8877db1d059",
+                  "fltt": "2", "invt": "2", "fields": "f14,f3,f12,f2",
+                  "secids": ",".join(df["mark"]) + ",?v=08926209912590994", }
+        url = "https://push2.eastmoney.com/api/qt/ulist.np/get"
+        res = requests.request(method='get', url=url, params=params)
+
+        # 3. 解析封装数据
+        data = res.json()["data"]["diff"]
+        rename = {'f2': 'price', 'f3': 'change_pct', 'f12': 'stock_code', 'f14': 'short_name', }
+        rank_df = pd.DataFrame(data).rename(columns=rename)
+        rank_df["change_pct"] = pd.to_numeric(rank_df["change_pct"], errors="coerce")
+        rank_df["price"] = pd.to_numeric(rank_df["price"], errors="coerce")
+        rank_df["change"] = rank_df["price"] * rank_df["change_pct"] / 100
+        rank_df["rank"] = range(1, len(rank_df) + 1)
+        return rank_df[["rank", "stock_code", "short_name", "price", "change", "change_pct"]]
+
+    def hot_rank_100_ths(self):
+        """
+        同花顺热股100
+        https://dq.10jqka.com.cn/fuyao/hot_list_data/out/hot_list/v1/stock?stock_type=a&type=hour&list_type=normal """
+
+        # api_url ='https://dq.10jqka.com.cn/fuyao/hot_list_data/out/hot_list/v1/stock?stock_type=a&type=hour&list_type=normal'
+        # text = self._get_text(api_url, '1')
+        # if THS_IP_LIMIT_RES in text:
+        #     return Exception(THS_IP_LIMIT_MSG)
+        # print(text)
+        pass
+
+
+if __name__ == '__main__':
+    print(Hot().hot_rank_100_ths())
+    print(Hot().pop_rank_100_east())
