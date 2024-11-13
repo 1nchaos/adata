c098eeb9cb763e7e33e6effbb411cd1de704dd5a
1nchaos <yc369.cool@163.com>
2024-11-14 00:22:50 +0800

    add Concept flow

---
+++ adata/stock/market/concept_capital_flow/concept_flow.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+@desc: 概念资金流向
+
+东方财富：概念
+https://data.eastmoney.com/bkzj/gn.html
+@author: 1nchaos
+@time: 2024/11/14
+@log: change log
+"""
+
+
+class ConceptCapitalFlow(object):
+    """概念资金流向"""
+
+    def __init__(self) -> None:
+        super().__init__()
